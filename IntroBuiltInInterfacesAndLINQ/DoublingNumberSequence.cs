cc9bd084de11eb759eba31818d2304fbbc3ba52b
GET Academy <terje@kolderup.net>
2024-12-19 12:56:16 +0100

    x

---
+++ IntroBuiltInInterfacesAndLINQ/DoublingNumberSequence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace IntroBuiltInInterfacesAndLINQ
+{
+    internal class DoublingNumberSequence : IEnumerable<int>, IEnumerator<int>
+    {
+        private int _maxNumber;
+
+        // 1, 2, 4, 8, 16, 32, ...
+        public int Current { get; private set; }
+
+        public DoublingNumberSequence(int startNumber, int maxNumber)
+        {
+            _maxNumber = maxNumber;
+            Current = startNumber;
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            if (Current >= _maxNumber) return false;
+            Current *= 2;
+            return true;
+        }
+
+        public void Reset()
+        {
+        }
+
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- IntroBuiltInInterfacesAndLINQ/Program.cs
+++ IntroBuiltInInterfacesAndLINQ/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using IntroBuiltInInterfacesAndLINQ;
+
 Console.WriteLine("Hello, World!");
+
+
+var ints1 = new List<int>{1, 2, 3};
+var ints2 = new int[]{1, 2, 3};
+string text = "jkfdskj";
+bool b = false;
+var sequence = new DoublingNumberSequence(1, 1000);
+
+foreach (var element in sequence)
+{
+    Console.WriteLine(element);
+}
